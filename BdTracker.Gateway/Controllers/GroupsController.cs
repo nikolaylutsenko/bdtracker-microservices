ec0550f3f6daeddf9fbefc96fa4cb05a60ef2184
Mykola Lutsenko <mykola.lutsenko@dataart.com>
2023-02-22 23:52:22 +0200

    Added calls to group services

--- BdTracker.Gateway/Controllers/GroupsController.cs
+++ BdTracker.Gateway/Controllers/GroupsController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Net.Http.Headers;
@@ -18,21 +20,58 @@ public class GroupsController : ControllerBase
     }
 
     [HttpGet]
-    public IActionResult GetAllGroups()
+    public async Task<IActionResult> GetAllGroups()
     {
-        var httpRequest = new HttpRequestMessage(
-            HttpMethod.Get,
-            "here must be an address")
+        var httpRequest = new HttpRequestMessage(HttpMethod.Get, "http://localhost:7771/groups") // TODO: move it in separate file
         {
             Headers =
             {
                 {HeaderNames.Accept, "application/json"},
-                {HeaderNames.UserAgent, "HttpRequestSample"}
+                //{HeaderNames.UserAgent, "HttpRequestSample"}
             }
         };
 
         var httpClient = _httpClientFactory.CreateClient();
+        var httpResponseMessage = await httpClient.SendAsync(httpRequest);
 
-        return Ok();
+        httpResponseMessage.EnsureSuccessStatusCode();
+
+        await using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync();
+
+        var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+        options.Converters.Add(new JsonStringEnumConverter(allowIntegerValues: false));
+
+        var result = await JsonSerializer.DeserializeAsync<IEnumerable<GroupResponse>>(contentStream, options);
+
+        return Ok(result);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var httpRequest = new HttpRequestMessage(HttpMethod.Get, $"http://localhost:7771/groups/{id}") // TODO: move it in separate file
+        {
+            Headers =
+            {
+                {HeaderNames.Accept, "application/json"},
+                //{HeaderNames.UserAgent, "HttpRequestSample"}
+            }
+        };
+
+        var httpClient = _httpClientFactory.CreateClient();
+        var httpResponseMessage = await httpClient.SendAsync(httpRequest);
+
+        httpResponseMessage.EnsureSuccessStatusCode();
+
+        await using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync();
+
+        var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+        options.Converters.Add(new JsonStringEnumConverter(allowIntegerValues: false));
+
+        var result = await JsonSerializer.DeserializeAsync<GroupResponse>(contentStream, options);
+
+        return Ok(result);
     }
 }
+
+public record GroupResponse(Guid Id, string Name);
